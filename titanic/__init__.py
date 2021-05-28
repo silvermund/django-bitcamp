2f59b1e2c68af0af3eb262ec007b1c992d9d77e6
silvermund <cliferd@naver.com>
2021-05-28 15:51:34 +0900

    16:00

---
+++ titanic/__init__.py
@@ -0,0 +1,12 @@
+from titanic.views.controller import Controller
+
+if __name__ == '__main__':
+    controller = Controller()
+    while 1:
+        menu = input('0-exit 1-preprocess')
+        if menu == '0':
+            break
+        elif menu == '1':
+            controller.preprocess('train.csv')
+        else:
+            continue
