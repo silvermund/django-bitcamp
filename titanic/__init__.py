9de1b9324c2309f8e2eab3de5ef2df526ccf9765
silvermund <cliferd@naver.com>
2021-05-31 17:45:31 +0900

    18:00

--- titanic/__init__.py
+++ titanic/__init__.py
@@ -1,12 +1,26 @@
 from titanic.views.controller import Controller
+from titanic.templates.plot import Plot
 
 if __name__ == '__main__':
-    controller = Controller()
     while 1:
-        menu = input('0-exit 1-preprocess')
+        menu = input('0-exit 1-data visualization\n'
+                     ' 2-modeling\n'
+                     ' 3-machine learning\n'
+                     ' 4-machine release ')
         if menu == '0':
             break
         elif menu == '1':
-            controller.preprocess('train.csv')
+            plot = Plot('train.csv')
+            plot.draw_survived_dead()
+            plot.draw_pclass()
+            plot.draw_sex()
+            plot.draw_embarked()
+        elif menu == '2':
+            controller = Controller()
+            controller.preprocess('train.csv', 'test.csv')
+        elif menu == '3':
+            pass
+        elif menu == '4':
+            pass
         else:
             continue
--- titanic/models/service.py
+++ titanic/models/service.py
@@ -5,8 +5,22 @@ class Service(object):
 
     dataset = Dataset()
 
-    def new_model(self, payload):
+    def new_model(self, payload) -> object:
         this = self.dataset
         this.context = './data/'
         this.fname = payload
         return pd.read_csv(this.context + this.fname)
+
+    @staticmethod
+    def create_train(this) -> object:
+        return this.train.drop('Survived', axis = 1)
+
+    @staticmethod
+    def create_label(this):
+        return this.train['Survived']
+
+    @staticmethod
+    def drop_feature(this, feature) -> object:
+        this.train = this.train.drop([feature], axis = 1)
+        this.test = this.test.drop([feature], axis = 1)
+        return this
