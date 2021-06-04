bc21d6254f865e5707eff40fd5ed696444a79844
silvermund <cliferd@naver.com>
2021-06-04 17:19:15 +0900

    18:00

--- samsung/samsung2.py
+++ samsung/samsung2.py
@@ -12,14 +12,16 @@ class Samsung2(object):
     driver_path = 'C:/Program Files/Google/Chrome/chromedriver'
     dic = {}
     ls = []
+    day = []
+    price = []
     df = None
 
     def scrap(self):
         driver = webdriver.Chrome(self.driver_path)
         driver.get(self.url)
         soup = BeautifulSoup(driver.page_source, 'lxml')
-        ls1 = soup.find_all('tc', {'class': 'tc'})
-        ls2 = soup.find_all('tc', {'class': 'num'})
+        ls1 = soup.find_all('td', {'class': 'tc'})
+        ls2 = soup.find_all('td', {'class': 'num'})
         for i in ls1:
             self.day.append(i.find('span').text)
         for i in ls2:
@@ -34,11 +36,11 @@ class Samsung2(object):
         self.df = pd.DataFrame.from_dict(dt, orient='index')
         print(self.df)
 
-    def df_to_csv(self):
-        path = './data/samsung2.csv'
-        self.df.to_csv(path, sep=',', na_rep='NaN')
+    # def df_to_csv(self):
+    #     path = './data/samsung2.csv'
+    #     self.df_to_csv(path, sep=',', na_rep='NaN')
 
 Samsung2().scrap()
 Samsung2().scrap_to_dict()
 Samsung2().dict_to_dataframe()
-Samsung2().df_to_csv()
+# Samsung2().df_to_csv()
