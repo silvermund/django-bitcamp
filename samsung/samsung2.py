ecf407350f2cd028d13bbcdf8063a06a8ef78519
silvermund <cliferd@naver.com>
2021-06-07 19:24:14 +0900

    samsung

--- samsung/samsung2.py
+++ samsung/samsung2.py
@@ -12,6 +12,8 @@ class Samsung2(object):
     driver_path = 'C:/Program Files/Google/Chrome/chromedriver'
     dic = {}
     ls = []
+    ls1 = []
+    ls2 = []
     day = []
     price = []
     df = None
@@ -20,12 +22,25 @@ class Samsung2(object):
         driver = webdriver.Chrome(self.driver_path)
         driver.get(self.url)
         soup = BeautifulSoup(driver.page_source, 'lxml')
-        ls1 = soup.find_all('td', {'class': 'tc'})
-        ls2 = soup.find_all('td', {'class': 'num'})
-        for i in ls1:
-            self.day.append(i.find('span').text)
-        for i in ls2:
-            self.price.append(i.find('span').text)
+        soup = soup.find_all(name='tr', attrs=({'onmouseover':'mouseOver(this)'}))
+
+        for i in soup:
+            ls1 = i.find_all('td')[0].span.text
+            ls2 = i.find_all('td')[1].span.text
+
+        self.day = ls1
+        self.price = ls2
+
+
+        # ls1 = soup.find_all('td', {'class': 'tc'})
+
+        # ls2 = soup.find_all('td')[1].text
+        # for i in ls2:
+        #     print(i)
+        # for i in ls1:
+        #     self.day.append(i.find('span').text)
+        # for i in ls2:
+        #     self.price.append(i.find('span').text)
 
     def scrap_to_dict(self):
         dic = dict(zip(self.day, self.price))
@@ -36,11 +51,15 @@ class Samsung2(object):
         self.df = pd.DataFrame.from_dict(dt, orient='index')
         print(self.df)
 
-    # def df_to_csv(self):
-    #     path = './data/samsung2.csv'
-    #     self.df_to_csv(path, sep=',', na_rep='NaN')
+    def df_to_csv(self):
+        path = './data/samsung2.csv'
+        self.df_to_csv(path, sep=',', na_rep='NaN')
+        #
+        # def df_to_csv(self):
+        #     path = './data/samsung2.csv'
+        #     self.df_to_csv(path, sep=',', na_rep='NaN')
 
 Samsung2().scrap()
 Samsung2().scrap_to_dict()
 Samsung2().dict_to_dataframe()
-# Samsung2().df_to_csv()
+Samsung2().df_to_csv()
